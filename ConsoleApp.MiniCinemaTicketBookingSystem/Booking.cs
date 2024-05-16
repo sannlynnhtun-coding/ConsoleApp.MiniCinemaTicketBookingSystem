52230cf9c7558c807d46348da1afbc4fddb298b3
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-16 23:57:01 +0630

    [Complete] Booking

---
+++ ConsoleApp.MiniCinemaTicketBookingSystem/Booking.cs
@@ -0,0 +1,15 @@
+﻿public class Booking
+{
+    public Movie Movie { get; set; }
+    public DateTime Showtime { get; set; }
+    public int SeatNumber { get; set; }
+    public string CustomerName { get; set; }
+
+    public Booking(Movie movie, DateTime showtime, int seatNumber, string customerName)
+    {
+        Movie = movie;
+        Showtime = showtime;
+        SeatNumber = seatNumber;
+        CustomerName = customerName;
+    }
+}
