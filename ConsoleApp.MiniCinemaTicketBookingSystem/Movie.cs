4a6131e56bb954541d0b3ef810ed7bd59122df1e
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-16 23:56:49 +0630

    [Complete] Movie

---
+++ ConsoleApp.MiniCinemaTicketBookingSystem/Movie.cs
@@ -0,0 +1,17 @@
+﻿public class Movie
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public List<DateTime> Showtimes { get; set; }
+    public int Seats { get; set; }
+    public int AvailableSeats { get; set; }
+
+    public Movie(string title, string description, List<DateTime> showtimes, int seats)
+    {
+        Title = title;
+        Description = description;
+        Showtimes = showtimes;
+        Seats = seats;
+        AvailableSeats = seats;
+    }
+}
